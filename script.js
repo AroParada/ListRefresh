b17a7b0528bea80d9f5ca0e2c7f9d26f2a317310
Aro Parada <haroldparada20@gmail.com>
2023-12-23 21:27:40 -0500

    can add to list but duplicates

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-function addToList(t) {
-    console.log(t.value)
-}
-
+// Crosses out done items
 var list = document.querySelector('ul');
 list.addEventListener("click",function(event) {
     if(event.target.tagName === 'LI') {
         event.target.classList.toggle('checked')
     }
 }, false);
+
+
+let data = [];
+
+document.getElementById("button-addon2").onclick = function () {
+    
+  var text = document.getElementById("input").value; //.value gets input values
+  data.push(text)
+  
+  for (i = 0; i < data.length; ++i) {
+    let li = document.createElement("li");
+    li.innerText = data[i];
+    list.appendChild(li);
+  }
+};
